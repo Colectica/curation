455a68f0446b53d1d19b689fcd6cb5adf9385d3b
Jeremy Iverson <jeremy@colectica.com>
2021-05-24 12:42:07 -0500

    Don't throw exceptions when PhysicalInstance cannot be loaded during a review task

--- src/Colectica.Curation.Web/Areas/Ddi/Controllers/CheckObservationCountController.cs
+++ src/Colectica.Curation.Web/Areas/Ddi/Controllers/CheckObservationCountController.cs
@@ -70,7 +70,7 @@ namespace Colectica.Curation.Web.Areas.Ddi.Controllers
                 }
                 catch (Exception ex)
                 {
-                    throw new HttpException(500, "Could not retrieve the PhysicalInstance for a file", ex);
+                    return View("~/Areas/Ddi/Views/CheckObservationCount/Details.cshtml", model);
                 }
             }
         }
