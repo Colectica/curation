8191459d7b36969b7592f085964ac6875be278da
Jeremy Iverson <jeremy@colectica.com>
2023-05-15 10:51:33 -0500

    Install service with 64 bit InstallUtil.exe

--- src/Colectica.Curation.Service/install.bat
+++ src/Colectica.Curation.Service/install.bat
@@ -1,6 +1,6 @@
 REM This should be run as an administrator
 REM You can control the service with the Administrator Tools-services
 
-"%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\installutil" "%~dp0Colectica.Curation.Service.exe"
+"%SystemRoot%\Microsoft.NET\Framework64\v4.0.30319\installutil" "%~dp0Colectica.Curation.Service.exe"
 
 pause
