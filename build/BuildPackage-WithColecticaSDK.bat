aa686978314864101a49cbf804e02927354c016c
Jeremy Iverson <jeremy@colectica.com>
2023-07-07 14:20:19 -0500

    Build script fix

--- build/BuildPackage-WithColecticaSDK.bat
+++ build/BuildPackage-WithColecticaSDK.bat
@@ -32,7 +32,7 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 
 REM CLI
 PUSHD ..\src\Colectica.Curation.Cli
-%msbuild% publish /P:Configuration=Release /P:Platform=AnyCPU 
+%msbuild% /P:Configuration=Release /P:Platform=AnyCPU 
 del bin\Release\net6.0\appsettings.json
 xcopy /e /y bin\Release\net6.0\ ..\..\dist\ColecticaCurationCli
 POPD
